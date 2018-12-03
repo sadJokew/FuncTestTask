00d29bec68792fb0cbcfec5b6f890498cb79adfd
sad november <sadnovember2@gmail.com>
2018-12-04 02:39:17 +0300

    func description

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -3,6 +3,17 @@ public class Function {
     Function(){
     }
 
+    /**
+     * @param a int
+     * @param b long
+     *          в различных комбинациях
+     *          (int, int) (int, long) (long, int) (long, long)
+     * @return данные типа java.lang.Integer или Void
+     *
+     * @throws UserException при некорректном типе данных
+     *  текст сообщения: "Error data type"
+     *
+     */
     public Object procedure(Object a, Object b){
         int tempInt = 1;
         long tempLong = 0L;
@@ -24,7 +35,7 @@ public class Function {
         } catch (UserException e) {
             e.printStackTrace();
         }
-        return Integer.MAX_VALUE;
+        return null;
     }
 
     String getName(Object a){
